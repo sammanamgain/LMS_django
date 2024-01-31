a9572f07a6d61f48138fb00501e433ed2a00d419
sammanamgain <amgain02@gmail.com>
2024-01-31 10:18:39 +0545

    migration of db

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Name=models.CharField(max_length=20)
+    Age=models.IntegerField(default=18)
+    subject=models.CharField(max_length=15)
+    
